2686d96d32bb4ebc3e894d9ffb984d54cbcfe078
J-P Nurmi <jpnurmi@gmail.com>
2014-11-11 23:17:41 +0100

    Split declaration & implementation

--- chanfilter.cpp
+++ chanfilter.cpp
@@ -24,214 +24,230 @@ public:
 		AddCommand("ListChans", static_cast<CModCommand::ModCmdFunc>(&CChanFilterMod::OnListChansCommand), "[client]", "List channels for a client.");
 	}
 
-	void OnAddClientCommand(const CString& line)
-	{
-		if (!AddClient(line.Token(1))) {
-			PutModule("Usage: AddClient <identifier>");
-			return;
-		}
-		OnListClientsCommand();
-	}
+	void OnAddClientCommand(const CString& line);
+	void OnDelClientCommand(const CString& line);
+	void OnListClientsCommand(const CString& = "");
+	void OnListChansCommand(const CString& line);
 
-	void OnDelClientCommand(const CString& line)
-	{
-		if (!DelClient(line.Token(1))) {
-			PutModule("Usage: DelClient <identifier>");
-			return;
-		}
-		OnListClientsCommand();
-	}
-
-	void OnListClientsCommand(const CString& = "")
-	{
-		const CString current = GetClient()->GetIdentifier();
-
-		CTable table;
-		table.AddColumn("Client");
-		table.AddColumn("Connected");
-		table.AddColumn("Hidden channels");
-		for (MCString::iterator it = BeginNV(); it != EndNV(); ++it) {
-			table.AddRow();
-			if (it->first == current)
-				table.SetCell("Client",  "*" + it->first);
-			else
-				table.SetCell("Client",  it->first);
-			table.SetCell("Connected", CString(GetNetwork()->FindClient(it->first)));
-			table.SetCell("Hidden channels", it->second.Ellipsize(128));
-		}
-
-		if (table.empty())
-			PutModule("No identified clients");
-		else
-			PutModule(table);
-	}
-
-	void OnListChansCommand(const CString& line)
-	{
-		const CString current = GetClient()->GetIdentifier();
-		const CString identifier = line.Token(1);
-
-		if (current.empty() && !identifier.empty() && FindNV(identifier) == EndNV()) {
-			PutModule("Unknown client: " + identifier);
-			return;
-		}
-
-		CTable table;
-		if (!identifier.empty())
-			table.AddColumn("Client");
-		table.AddColumn("Channel");
-		table.AddColumn("Status");
-
-		for (CChan* channel : GetNetwork()->GetChans()) {
-			table.AddRow();
-			if (!identifier.empty())
-				table.SetCell("Client", identifier);
-			table.SetCell("Channel", channel->GetName());
-			if (channel->IsDisabled())
-				table.SetCell("Status", "Disabled");
-			else if (channel->IsDetached())
-				table.SetCell("Status", "Detached");
-			else if (identifier.empty() || IsChannelVisible(identifier, channel->GetName()))
-				table.SetCell("Status", "Visible");
-			else
-				table.SetCell("Status", "Hidden");
-		}
-
-		PutModule(table);
-	}
-
-	virtual void OnClientLogin() override
-	{
-		AddClient(GetClient()->GetIdentifier());
-	}
-
-	virtual EModRet OnUserRaw(CString& line) override
-	{
-		CClient* client = GetClient();
-		const CString identifier = client->GetIdentifier();
-
-		if (!identifier.empty()) {
-			const CString cmd = line.Token(0);
-			if (cmd.Equals("JOIN")) {
-				const CString name = line.Token(1);
-				SetChannelVisible(identifier, name, true);
-				CChan* channel = client->GetNetwork()->FindChan(name);
-				if (channel) {
-					channel->JoinUser(true, "", client);
-					channel->SendBuffer(client);
-					return HALT;
-				}
-			} else if (cmd.Equals("PART")) {
-				const CString name = line.Token(1);
-				SetChannelVisible(identifier, name, false);
-				// bypass OnUserRaw()
-				client->Write(":" + client->GetNickMask() + " PART " + name + "\r\n");
-				return HALT;
-			}
-		}
-		return CONTINUE;
-	}
-
-	virtual EModRet OnSendToClient(CString& line, CClient& client) override
-	{
-		EModRet ret = CONTINUE;
-		CIRCNetwork* network = client.GetNetwork();
-		CString identifier = client.GetIdentifier();
-
-		if (network && !identifier.empty()) {
-			// discard message tags
-			CString msg = line;
-			if (msg.StartsWith("@"))
-				msg = msg.Token(1, true);
-
-			const CNick nick(msg.Token(0).TrimPrefix_n());
-			const CString cmd = msg.Token(1);
-			const CString rest = msg.Token(2, true);
-
-			if (cmd.Equals("QUIT") || cmd.Equals("NICK")) {
-				bool filter = true;
-				const SCString channels = GetHiddenChannels(identifier);
-				for (const CString& name : channels) {
-					const CChan* channel = network->FindChan(name);
-					if (channel && channel->FindNick(nick.GetNick()))
-						filter = false;
-				}
-				if (filter)
-					return CONTINUE;
-			}
-
-			CString channel;
-			if (cmd.length() == 3 && isdigit(cmd[0]) && isdigit(cmd[1]) && isdigit(cmd[2])) {
-				unsigned int num = cmd.ToUInt();
-				if (num == 353) // RPL_NAMES
-					channel = rest.Token(2);
-				else
-					channel = rest.Token(1);
-			} else if (cmd.Equals("NOTICE")) {
-				if (nick.NickEquals("ChanServ")) {
-					CString target = rest.Token(1).TrimPrefix_n(":[").TrimSuffix_n("]");
-					if (network->IsChan(target) && !IsChannelVisible(identifier, target))
-						return HALT;
-				}
-				channel = rest.Token(0);
-			} else if (cmd.Equals("PRIVMSG") || cmd.Equals("JOIN") || cmd.Equals("PART") || cmd.Equals("MODE") || cmd.Equals("KICK") || cmd.Equals("TOPIC")) {
-				channel = rest.Token(0);
-			}
-			channel.TrimPrefix(":");
-
-			if (network->IsChan(channel) && !IsChannelVisible(identifier, channel))
-				ret = HALT;
-
-			if (cmd.Equals("PART") && nick.GetNick().Equals(client.GetNick()))
-				SetChannelVisible(identifier, channel, false);
-		}
-		return ret;
-	}
+	virtual void OnClientLogin() override;
+	virtual EModRet OnUserRaw(CString& line) override;
+	virtual EModRet OnSendToClient(CString& line, CClient& client) override;
 
 private:
-	SCString GetHiddenChannels(const CString& identifier) const
-	{
-		SCString channels;
-		GetNV(identifier).Split(",", channels);
-		return channels;
-	}
+	SCString GetHiddenChannels(const CString& identifier) const;
+	bool IsChannelVisible(const CString& identifier, const CString& channel) const;
+	void SetChannelVisible(const CString& identifier, const CString& channel, bool visible);
 
-	bool IsChannelVisible(const CString& identifier, const CString& channel) const
-	{
-		const SCString channels = GetHiddenChannels(identifier);
-		return channels.find(channel.AsLower()) == channels.end();
-	}
-
-	void SetChannelVisible(const CString& identifier, const CString& channel, bool visible)
-	{
-		if (!identifier.empty()) {
-			SCString channels = GetHiddenChannels(identifier);
-			if (visible)
-				channels.erase(channel.AsLower());
-			else
-				channels.insert(channel.AsLower());
-			SetNV(identifier, CString(",").Join(channels.begin(), channels.end()));
-		}
-	}
-
-	bool AddClient(const CString& identifier)
-	{
-		if (!identifier.empty()) {
-			SetNV(identifier, GetNV(identifier));
-			return true;
-		}
-		return false;
-	}
-
-	bool DelClient(const CString& identifier)
-	{
-		if (!identifier.empty()) {
-			DelNV(identifier);
-			return true;
-		}
-		return false;
-	}
+	bool AddClient(const CString& identifier);
+	bool DelClient(const CString& identifier);
 };
+
+void CChanFilterMod::OnAddClientCommand(const CString& line)
+{
+	if (!AddClient(line.Token(1))) {
+		PutModule("Usage: AddClient <identifier>");
+		return;
+	}
+	OnListClientsCommand();
+}
+
+void CChanFilterMod::OnDelClientCommand(const CString& line)
+{
+	if (!DelClient(line.Token(1))) {
+		PutModule("Usage: DelClient <identifier>");
+		return;
+	}
+	OnListClientsCommand();
+}
+
+void CChanFilterMod::OnListClientsCommand(const CString&)
+{
+	const CString current = GetClient()->GetIdentifier();
+
+	CTable table;
+	table.AddColumn("Client");
+	table.AddColumn("Connected");
+	table.AddColumn("Hidden channels");
+	for (MCString::iterator it = BeginNV(); it != EndNV(); ++it) {
+		table.AddRow();
+		if (it->first == current)
+			table.SetCell("Client",  "*" + it->first);
+		else
+			table.SetCell("Client",  it->first);
+		table.SetCell("Connected", CString(GetNetwork()->FindClient(it->first)));
+		table.SetCell("Hidden channels", it->second.Ellipsize(128));
+	}
+
+	if (table.empty())
+		PutModule("No identified clients");
+	else
+		PutModule(table);
+}
+
+void CChanFilterMod::OnListChansCommand(const CString& line)
+{
+	const CString current = GetClient()->GetIdentifier();
+	const CString identifier = line.Token(1);
+
+	if (current.empty() && !identifier.empty() && FindNV(identifier) == EndNV()) {
+		PutModule("Unknown client: " + identifier);
+		return;
+	}
+
+	CTable table;
+	if (!identifier.empty())
+		table.AddColumn("Client");
+	table.AddColumn("Channel");
+	table.AddColumn("Status");
+
+	for (CChan* channel : GetNetwork()->GetChans()) {
+		table.AddRow();
+		if (!identifier.empty())
+			table.SetCell("Client", identifier);
+		table.SetCell("Channel", channel->GetName());
+		if (channel->IsDisabled())
+			table.SetCell("Status", "Disabled");
+		else if (channel->IsDetached())
+			table.SetCell("Status", "Detached");
+		else if (identifier.empty() || IsChannelVisible(identifier, channel->GetName()))
+			table.SetCell("Status", "Visible");
+		else
+			table.SetCell("Status", "Hidden");
+	}
+
+	PutModule(table);
+}
+
+void CChanFilterMod::OnClientLogin()
+{
+	AddClient(GetClient()->GetIdentifier());
+}
+
+CModule::EModRet CChanFilterMod::OnUserRaw(CString& line)
+{
+	CClient* client = GetClient();
+	const CString identifier = client->GetIdentifier();
+
+	if (!identifier.empty()) {
+		const CString cmd = line.Token(0);
+		if (cmd.Equals("JOIN")) {
+			const CString name = line.Token(1);
+			SetChannelVisible(identifier, name, true);
+			CChan* channel = client->GetNetwork()->FindChan(name);
+			if (channel) {
+				channel->JoinUser(true, "", client);
+				channel->SendBuffer(client);
+				return HALT;
+			}
+		} else if (cmd.Equals("PART")) {
+			const CString name = line.Token(1);
+			SetChannelVisible(identifier, name, false);
+			// bypass OnUserRaw()
+			client->Write(":" + client->GetNickMask() + " PART " + name + "\r\n");
+			return HALT;
+		}
+	}
+	return CONTINUE;
+}
+
+CModule::EModRet CChanFilterMod::OnSendToClient(CString& line, CClient& client)
+{
+	EModRet ret = CONTINUE;
+	CIRCNetwork* network = client.GetNetwork();
+	CString identifier = client.GetIdentifier();
+
+	if (network && !identifier.empty()) {
+		// discard message tags
+		CString msg = line;
+		if (msg.StartsWith("@"))
+			msg = msg.Token(1, true);
+
+		const CNick nick(msg.Token(0).TrimPrefix_n());
+		const CString cmd = msg.Token(1);
+		const CString rest = msg.Token(2, true);
+
+		if (cmd.Equals("QUIT") || cmd.Equals("NICK")) {
+			bool filter = true;
+			const SCString channels = GetHiddenChannels(identifier);
+			for (const CString& name : channels) {
+				const CChan* channel = network->FindChan(name);
+				if (channel && channel->FindNick(nick.GetNick()))
+					filter = false;
+			}
+			if (filter)
+				return CONTINUE;
+		}
+
+		CString channel;
+		if (cmd.length() == 3 && isdigit(cmd[0]) && isdigit(cmd[1]) && isdigit(cmd[2])) {
+			unsigned int num = cmd.ToUInt();
+			if (num == 353) // RPL_NAMES
+				channel = rest.Token(2);
+			else
+				channel = rest.Token(1);
+		} else if (cmd.Equals("NOTICE")) {
+			if (nick.NickEquals("ChanServ")) {
+				CString target = rest.Token(1).TrimPrefix_n(":[").TrimSuffix_n("]");
+				if (network->IsChan(target) && !IsChannelVisible(identifier, target))
+					return HALT;
+			}
+			channel = rest.Token(0);
+		} else if (cmd.Equals("PRIVMSG") || cmd.Equals("JOIN") || cmd.Equals("PART") || cmd.Equals("MODE") || cmd.Equals("KICK") || cmd.Equals("TOPIC")) {
+			channel = rest.Token(0);
+		}
+		channel.TrimPrefix(":");
+
+		if (network->IsChan(channel) && !IsChannelVisible(identifier, channel))
+			ret = HALT;
+
+		if (cmd.Equals("PART") && nick.GetNick().Equals(client.GetNick()))
+			SetChannelVisible(identifier, channel, false);
+	}
+	return ret;
+}
+
+SCString CChanFilterMod::GetHiddenChannels(const CString& identifier) const
+{
+	SCString channels;
+	GetNV(identifier).Split(",", channels);
+	return channels;
+}
+
+bool CChanFilterMod::IsChannelVisible(const CString& identifier, const CString& channel) const
+{
+	const SCString channels = GetHiddenChannels(identifier);
+	return channels.find(channel.AsLower()) == channels.end();
+}
+
+void CChanFilterMod::SetChannelVisible(const CString& identifier, const CString& channel, bool visible)
+{
+	if (!identifier.empty()) {
+		SCString channels = GetHiddenChannels(identifier);
+		if (visible)
+			channels.erase(channel.AsLower());
+		else
+			channels.insert(channel.AsLower());
+		SetNV(identifier, CString(",").Join(channels.begin(), channels.end()));
+	}
+}
+
+bool CChanFilterMod::AddClient(const CString& identifier)
+{
+	if (!identifier.empty()) {
+		SetNV(identifier, GetNV(identifier));
+		return true;
+	}
+	return false;
+}
+
+bool CChanFilterMod::DelClient(const CString& identifier)
+{
+	if (!identifier.empty()) {
+		DelNV(identifier);
+		return true;
+	}
+	return false;
+}
 
 template<> void TModInfo<CChanFilterMod>(CModInfo& Info)
 {
